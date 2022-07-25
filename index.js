8cbe20eb3d1fc01ef0af515eae85bdf59cf3f840
Fallanagel <havemedance@hotmail.com>
2022-07-24 21:19:21 -0500

    adding 404 page

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-require('dotenv').config()
+rrequire('dotenv').config()
 const express = require('express')
 const app = express()
+
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.render('home')
 })
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    res.render('error404')
+})
+
+
+app.get('*', (req, res) => {
+    res.send('404 page')
 })
 
 app.listen(process.env.PORT)
+
 
 
