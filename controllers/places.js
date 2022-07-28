fea8da42ddd4e73b4df825e443d0a9515d8a134f
Fallanagel <havemedance@hotmail.com>
2022-07-27 20:40:46 -0500

    adding delete functionality implemeneted

--- controllers/places.js
+++ controllers/places.js
@@ -8,19 +8,21 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
-router.get('/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
     res.render('error404')
   }
   else if (!places[id]) {
-    res.render('places/show', { place: places[id] })
-
+    res.render('error404')
   }
   else {
-    res.render('places/show')
+    places.splice(id, 1)
+    res.redirect('/places')
   }
 })
+
+
 
 
 module.exports = router
