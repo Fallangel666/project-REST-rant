a47e249f26663771294fbdd7d38692380457dd7c
Fallanagel <havemedance@hotmail.com>
2022-08-08 19:24:49 -0500

    added comment.js

--- index.js
+++ index.js
@@ -3,18 +3,8 @@ require('dotenv').config()
 const express = require('express')
 const methodOverride = require('method-override')
 const app = express()
-const mongoose = require('mongoose')
-const mongoose = require('mongoose')
-
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true, 
-  useUnifiedTopology: true
-})
-
-module.exports.Place = require('./places')
 
 // Express Settings
-app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,65 +1,38 @@
 const React = require('react')
-const Def = require('./default')
+const Def = require('../default')
 
-function index(data) {
-    let placesFormatted = data.places.map((place, index) => {
-        return (
-            <div key={'index'} className="col-sm-6">
-                <h2>
-                  <a href={/places/`${place.id}`}>
-                  {place.name}</a></h2>
-                <img src={place.pic} alt={place.name} />
-            </div>
-        )
+function index (data) {
+    let placesFormatted = data.places.map((place) => {
+      return (
+        <div className="col-sm-6">
+          <h2>
+            <a href={`/places/${place.id}`} >
+              {place.name}
+            </a>
+          </h2>
+          <p className="text-center">
+            {place.cuisines}
+          </p>
+          <img src={place.pic} alt={place.name} />
+          <p className="text-center">
+            Located in {place.city}, {place.state}
+          </p>
+        </div>
+      )
     })
     return (
-        <Def>
-            <main>
-                <h1>Places to Rant or Rave About</h1>
-                <div className="row">
-                    {placesFormatted}
-                </div>
-            </main>
-        </Def>
+      <Def>
+          <main>
+              <h1>Places to Rant or Rave About</h1>
+              <div className="row">
+                {placesFormatted}
+              </div>
+          </main>
+      </Def>
     )
-}
+  }
 
-let placesFormatted = data.places.map((place, index) => {
-    return (
-      <div className="col-sm-6">
-        <h2>
-          <a href={`/places/${index}`} >
-            {place.name}
-          </a>
-        </h2>
-        <p className="text-center">
-          {place.cuisines}
-        </p>
-        <img src={place.pic} alt={place.name} />
-        <p className="text-center">
-          Located in {place.city}, {place.state}
-        </p>
-      </div>
-    )
-  })
-  
-
-res.render('places/index', { places })
-
-let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: 'public/images/kyle-johnson-TEZZzuQTt8g-unsplash.jpg'
-}, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: 'public/images/ishan-seefromthesky-TobZaa8ZwI4-unsplash.jpg'
-}]
-
+module.exports = index
 
 
 
